4326367106f2fdc302b7923303b0354d6c1d5e7a
Gabriela Porto <gporto@imersys.com>
2021-01-28 20:02:55 -0300

    Bug fixed sound

--- after-porre/src/components/BotaoStart.vue
+++ after-porre/src/components/BotaoStart.vue
@@ -1,15 +1,16 @@
 <template>
-    <v-btn v-on:click="playsound('./mlem.mp3')">MLEM</v-btn>
+  <v-btn v-on:click="playsound('http://www.sousound.com/music/healing/healing_01.mp3')">MLEM</v-btn>
 </template>
 <script>
 export default {
     data: () => ({
     }),
     methods: {
-      playsound: function () {
-  var sound = new Audio('mlem.mp3');
-  sound.play();
-}
-    }
+      playsound: function (nomeArquivo) {
+        var sound = new Audio(nomeArquivo);
+        console.log(sound)
+        sound.play();
+        }
+      }
     }
 </script>
