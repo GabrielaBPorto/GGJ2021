91267d76d292d91d5528207310c27373eca4fb5b
Gabriela Porto <gporto@imersys.com>
2021-01-29 22:17:51 -0300

    Router Added

---
+++ after-porre/src/components/Cena.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container fluid>
+        <v-row justify="center" align="end">
+            <v-col cols="12" sm="6" justify="center" align="end">
+                Cenas
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+
+export default {
+    data: () => ({
+        
+    }),
+
+  }
+</script>
