3624582d9159a87898c1c8feabbf2a27712802b5
CesarCamillo <cesar.a.a.camillo@gmail.com>
2021-01-30 12:59:50 -0300

    Mais valores e dicts no state

--- after-porre/src/store/state.js
+++ after-porre/src/store/state.js
@@ -2,6 +2,8 @@
 
 export default () => ({
     valor: 0,
+
+    // Músicas & Efeitos Sonoros
     sounds: [
       { id: 'inicio_inicio', path: require('@/assets/sounds/inicio_inicio.mp3')},
       { id: 'inicio_loop', path: require('@/assets/sounds/inicio_loop.mp3')},
@@ -14,8 +16,12 @@ export default () => ({
       { id: 'externa_dia_inicio', path: require('@/assets/sounds/externa_dia_inicio.mp3')},
       { id: 'externa_dia_loop', path: require('@/assets/sounds/externa_dia_loop.mp3')},
       { id: 'melhor_amigo_inicio', path: require('@/assets/sounds/melhor_amigo_inicio.mp3')},
-      { id: 'melhor_amigo_loop', path: require('@/assets/sounds/melhor_amigo_loop.mp3')}
+      { id: 'melhor_amigo_loop', path: require('@/assets/sounds/melhor_amigo_loop.mp3')},
+      { id: 'interesse_romantico_inicio', path: require('@/assets/sounds/interesse_romantico_inicio.mp3')},
+      { id: 'interesse_romantico_loop',   path: require('@/assets/sounds/interesse_romantico_loop.mp3')},
     ],
+
+    // Imagens
     images: [
       // Ícones & Objetos
       { id: 'icone_celular', path: require('@/assets/images/icone_celular.png')},
@@ -59,5 +65,37 @@ export default () => ({
       { id: 'quarto_dia', path: require('@/assets/images/cenario/quarto_dia.png')},
       { id: 'quarto_manha', path: require('@/assets/images/cenario/quarto_manha.png')},
       { id: 'quarto_noite', path: require('@/assets/images/cenario/quarto_noite.png')}
-  ] 
+    ],
+
+    // Diálogos
+    dialogs: [
+      { cena: 1, sequencia: 1, personagem: 'narrador', text: 'Você acorda grunhindo em sua cama, olhos sensíveis a pouca luz do Sol entrando no quarto.'},
+      { cena: 1, sequencia: 2, personagem: 'celular', text: 'Uuuugggghhh... que dor de cabeça... será que alguém anotou a placa do ônibus que me atropelou?'},
+      { cena: 1, sequencia: 3, personagem: 'mc', text: 'A propósito, como será que eu cheguei em casa?'},
+      { cena: 1, sequencia: 4, personagem: 'narrador', text: 'Você coloca as mãos na cabeça, massageando suas tempôras para fazer passar a dor'},
+      { cena: 1, sequencia: 5, personagem: 'mc', text: 'Não consigo lembrar de nada que aconteceu ontem a noite... e já é tarde, mal entra luz pela janela...'},      
+      { cena: 1, sequencia: 6, personagem: 'mc', text: 'Preciso achar meus óculos, não estou enxergando nada.'},
+
+      { cena: 1, sequencia: 7, personagem: 'óculos', text: 'Droga, se esse é meu óculos antigo, onde será que está o novo? Bom, melhor que nada.'},
+      { cena: 1, sequencia: 7, personagem: 'celular_blur', text: 'É, sem meus óculos não vou conseguir ler nada...'},
+      { cena: 1, sequencia: 7, personagem: 'cueca_blur', text: 'O que diabos será que é isso?'},
+      
+      { cena: 1, sequencia: 8, personagem: 'cueca', text: '.... de quem é essa cueca e como ela veio parar aqui?'},
+      { cena: 1, sequencia: 8, personagem: 'mc', text: 'Quantas mensagens não lidas...'},
+    ],
+    
+    msgs_cel: [
+      { cena: 1, msg: 1, personagem: 'melhor_amigo', time: '04:07', text: 'Kdê vcê?????'},
+      { cena: 1, msg: 2, personagem: 'melhor_amigo', time: '05:11', text: 'Te perdi na festa e já voltei pra casa.' },
+      { cena: 1, msg: 3, personagem: 'melhor_amigo', time: '05:12', text: 'Avisa quando chegar em casa!'},
+      { cena: 1, msg: 4, personagem: 'melhor_amigo', time: '13:58', text: 'Já acordou?????'},
+      { cena: 1, msg: 5, personagem: 'melhor_amigo', time: '15:33', text: 'Alou???'},
+      { cena: 1, msg: 6, personagem: 'melhor_amigo', time: '16:00', text: 'Você tem duas ligações perdidas'}
+    ],
+
+    // Estados Internos do Jogo
+    game_start: false,
+    oculos: 0,
+    notif_celular: false,
+    cena: 0
 })
